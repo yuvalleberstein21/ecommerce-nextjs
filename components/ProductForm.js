a4241f7cbf6b4a226f7cb45329a2750830526e90
yuvalleberstein21 <94398909+yuvalleberstein21@users.noreply.github.com>
2023-06-29 16:05:58 +0300

    update

---
+++ components/ProductForm.js
@@ -0,0 +1,106 @@
+import axios from "axios";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+
+
+export default function ProductForm({
+    _id,
+    title: existingTitle,
+    description: existingDescription,
+    price: existingPrice,
+    images,
+}) {
+
+
+    const [title, setTitle] = useState(existingTitle || '');
+    const [description, setDescription] = useState(existingDescription || '');
+    const [price, setPrice] = useState(existingPrice || '');
+
+    const [goToProducts, setGoToProducts] = useState(false)
+
+    const router = useRouter();
+
+    const saveProduct = async (e) => {
+        e.preventDefault();
+
+        const data = { title, description, price }
+
+        if (_id) {
+            //update
+            await axios.put('/api/products', { ...data, _id });
+
+        } else {
+            //create
+            await axios.post('/api/products', data);
+
+        }
+        setGoToProducts(true)
+    }
+    if (goToProducts) {
+        router.push('/products')
+    }
+
+    const uploadImages = async (e) => {
+        const files = e.target?.files;
+        if (files?.length > 0) {
+            const data = new FormData();
+            for (const file of files) {
+                data.append('file', file)
+            }
+            const res = await fetch('/api/upload', {
+                method: 'POST',
+                body: data
+            })
+            console.log(res);
+        }
+    }
+
+    return (
+
+        <form onSubmit={saveProduct}>
+
+            <label>Product name</label>
+            <input
+                type="text"
+                placeholder="product name"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+            />
+            <label>Photos</label>
+            <div className="mb-2">
+                <label className="w-24 cursor-pointer h-24 text-center flex items-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+                    </svg>
+                    <div>
+                        Upload
+                    </div>
+                    <input type="file" className="hidden" onChange={uploadImages} />
+                </label>
+                {!images?.length && (
+                    <div>No photos in this product</div>
+                )}
+            </div>
+            <label>Description</label>
+            <textarea
+                placeholder="description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+            />
+            <label>Price (in USD)</label>
+            <input
+                type="number"
+                placeholder="price"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+            />
+
+            <button
+                type="submit"
+                className="btn-primary">
+                Save
+            </button>
+        </form>
+
+    );
+}
--- models/Product.js
+++ models/Product.js
@@ -1,7 +1,12 @@
-import {Schema} from "mongoose"
+import { Schema, model, models } from "mongoose"
 
-const ModelSchema = new Schema({
-    title: {type:String, required: true},
+const ProductSchema = new Schema({
+    title: { type: String, required: true },
     description: String,
-    price: {type: Number, required: true}
-})
+    price: { type: Number, required: true }
+});
+
+export const Product = models.Product || model('Product', ProductSchema);
+
+
+
